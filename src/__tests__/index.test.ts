094ab2284f5463ea0c1c190e3d4c24edf878eacb
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-11-08 07:49:02 -0800

    pre publish

--- src/__tests__/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { sum } from '../index';
-
-describe('sum', () => {
-  it('should add two numbers correctly', () => {
-    expect(sum(1, 2)).toBe(3);
-    expect(sum(-1, 1)).toBe(0);
-    expect(sum(0, 0)).toBe(0);
-  });
-});
